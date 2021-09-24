720393b1902dd0d3d60c8a638c5d16f6e5ae4132
vviyo <3219699854@qq.com>
2021-09-24 15:30:38 +0800

    api test

--- src/main.js
+++ src/main.js
@@ -4,17 +4,31 @@ import App from './App.vue'
 
 // $mount
 // createApp(App).mount('#app')
-// 创建应用实例对象 --app(类似于之前Vue2中的vm.但是app比vm更轻)
+// 创建应用实例对象 --app
 const app = createApp(App)
 // 挂载
 app.mount('#app')
 
-// 取消挂载
-// setInterval(() => {
-//     app.unmount('#app')
-// }, 1000);
-console.log(app);
-// const vm = new Vue({
-//     render:(h) => h(App)
-// })
-// vm.$mount('#app')
+/*
+    取消挂载
+    setInterval(() => {
+        app.unmount('#app')
+    }, 1000);
+*/
+/*
+    app
+    类似于之前Vue2中的vm.但是app比vm更轻
+    挂载更少的方法和属性
+*/
+
+/*
+    vue2 写法
+    const vm = new Vue({
+        render:(h) => h(App)
+    })
+    vm.$mount('#app')
+*/
+
+// 引入axios
+// import axios from "axios";
+// app.prototype.$http = axios;
